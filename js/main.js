eed3e33a664f6720050bbef76327a89c5241e1da
Peter K.W <peter.k.w.96@hotmail.com>
2026-02-07 11:50:12 +0100

    Escape key now closes resume modal, added some comments

--- js/main.js
+++ js/main.js
@@ -2,16 +2,26 @@ var modal = document.getElementById("resumeModal");
 var resumeLink = document.getElementById("resumeLink");
 var span = document.getElementsByClassName("close-button")[0];
 
+// Open the modal when the user clicks on the resume link
 resumeLink.onclick = function() {
     modal.style.display = "block";
 }
 
+// Close the modal when the user clicks on the close button
 span.onclick = function() {
     modal.style.display = "none";
 }
 
+// Close the modal when the user clicks outside of it
 window.onclick = function(event) {
     if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Close the modal when the user presses the Escape key
+document.onkeydown = function(event) {
+    if (event.key === "Escape") {
         modal.style.display = "none";
     }
 }
